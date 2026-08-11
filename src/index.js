b907bd96c944d4190a957770289d10c6cc42c845
lethanh2006 <thanhld2006.ptit@gmail.com>
2026-08-11 18:15:36 +0700

    update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/health', (_req, res) => {
+  res.json({ status: 'ok', service: 'logger' });
+});
 
 const logger = winston.createLogger({
   level: 'info',
